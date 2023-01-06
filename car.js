a03a28ea4602f554f81e00fc07587bba38f89a7b
Anmol Verma <anmolverma102002@gmail.com>
2023-01-06 23:06:52 +0530

    feat: :sparkles: added movement physics

--- car.js
+++ car.js
@@ -12,6 +12,7 @@ class Car {
 		this.acceleration = 0.2;
 		this.maxSpeed = 3;
 		this.friction = 0.05;
+		this.angle = 0;
 
 		this.controls = new Controls();
 	}
@@ -28,31 +29,31 @@ class Car {
 		if (this.speed < -this.maxSpeed / 2) {
 			this.speed = -this.maxSpeed / 2;
 		}
-		if(this.speed>0){
-			this.speed-=this.friction;
+		if (this.speed > 0) {
+			this.speed -= this.friction;
 		}
-		if(this.speed<0){
-			this.speed+=this.friction;
+		if (this.speed < 0) {
+			this.speed += this.friction;
 		}
-		if(Math.abs(this.speed)<this.friction){
+		if (Math.abs(this.speed) < this.friction) {
 			this.speed = 0;
 		}
-		this.y -= this.speed;
 		if (this.controls.left) {
-			this.x -= 2;
+			this.angle += 0.03;
 		}
 		if (this.controls.right) {
-			this.x += 2;
+			this.angle -= 0.03;
 		}
+		this.x -= Math.sin(this.angle) * this.speed;
+		this.y -= Math.cos(this.angle) * this.speed;
 	}
 	draw(crx) {
+		crx.save();
+		crx.translate(this.x, this.y);
+		crx.rotate(-this.angle);
 		ctx.beginPath();
-		ctx.rect(
-			this.x - this.width / 2,
-			this.y - this.height / 2,
-			this.width,
-			this.height
-		);
+		ctx.rect(-this.width / 2, -this.height / 2, this.width, this.height);
 		ctx.fill();
+		ctx.restore();
 	}
 }
