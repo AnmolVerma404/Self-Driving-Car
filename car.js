a93e546fed193b3348d97e499c0ba5f03997be91
Anmol Verma <anmolverma102002@gmail.com>
2023-01-09 10:41:05 +0530

    feat: :sparkles: added sensors in car

--- car.js
+++ car.js
@@ -14,10 +14,12 @@ class Car {
 		this.friction = 0.05;
 		this.angle = 0;
 
+		this.sensor = new Sensors(this);
 		this.controls = new Controls();
 	}
 	update() {
 		this.#move();
+		this.sensor.update();
 	}
 	#move() {
 		if (this.controls.forward) {
@@ -61,5 +63,6 @@ class Car {
 		ctx.rect(-this.width / 2, -this.height / 2, this.width, this.height);
 		ctx.fill();
 		ctx.restore();
+		this.sensor.draw(ctx);
 	}
 }
