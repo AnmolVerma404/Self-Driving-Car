7119a3d9d3a862c6d2e20926bcc220055d7c01d1
Anmol Verma <anmolverma102002@gmail.com>
2023-01-10 15:58:27 +0530

    feat: :sparkles: added traffic collision detection

--- car.js
+++ car.js
@@ -14,24 +14,29 @@ class Car {
 		this.friction = 0.05;
 		this.angle = 0;
 		this.damaged = false;
-		if(controlType!="DUMMY"){
+		if (controlType != 'DUMMY') {
 			this.sensor = new Sensor(this);
 		}
 		this.controls = new Controls(controlType);
 	}
-	update(roadBorders) {
+	update(roadBorders, traffic) {
 		if (!this.damaged) {
 			this.#move();
 			this.polygon = this.#createPolygon();
-			this.damaged = this.#assessDamage(roadBorders);
+			this.damaged = this.#assessDamage(roadBorders, traffic);
 		}
-		if(this.sensor){
-			this.sensor.update(roadBorders);
+		if (this.sensor) {
+			this.sensor.update(roadBorders, traffic);
 		}
 	}
-	#assessDamage(roadBorders) {
+	#assessDamage(roadBorders, traffic) {
 		for (let i = 0; i < roadBorders.length; ++i) {
 			if (polyIntersect(this.polygon, roadBorders[i])) {
+				return true;
+			}
+		}
+		for (let i = 0; i < traffic.length; ++i) {
+			if (polyIntersect(this.polygon, traffic[i].polygon)) {
 				return true;
 			}
 		}
@@ -96,11 +101,11 @@ class Car {
 		this.x -= Math.sin(this.angle) * this.speed;
 		this.y -= Math.cos(this.angle) * this.speed;
 	}
-	draw(ctx) {
+	draw(ctx, color) {
 		if (this.damaged) {
 			ctx.fillStyle = 'gray';
 		} else {
-			ctx.fillStyle = 'black';
+			ctx.fillStyle = color;
 		}
 		ctx.beginPath();
 		ctx.moveTo(this.polygon[0].x, this.polygon[0].y);
@@ -108,7 +113,7 @@ class Car {
 			ctx.lineTo(this.polygon[i].x, this.polygon[i].y);
 		}
 		ctx.fill();
-		if(this.sensor){
+		if (this.sensor) {
 			this.sensor.draw(ctx);
 		}
 	}
